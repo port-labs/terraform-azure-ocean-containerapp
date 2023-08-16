d19ced0c474230dfd8c3b54736cbf30e8bf4cd77
tankilevitch <tomtankilevitch@gmail.com>
2023-08-16 13:27:02 +0300

    base implementation for azure containerapp terraform with examples

---
+++ examples/base/main.tf
@@ -0,0 +1,27 @@
+module "ocean_integration" {
+  source       = "../.."
+
+  # required port parameters so that the integration could communicate with Port
+  port = {
+    client_id     = var.port_client_id
+    client_secret = var.port_client_secret
+    base_url = var.port_base_url
+  }
+
+  initialize_port_resources = var.initialize_port_resources
+
+  # required port integration parameters so Port could identify the integration
+  integration = {
+    type       = "azure"
+    identifier = var.integration_identifier
+    config     = {
+    }
+  }
+  resource_group_name = var.resource_group_name
+  location = var.location
+
+  image = var.image
+
+  additional_secrets = var.additional_secrets
+  additional_environment_variables = var.additional_environment_variables
+}
---
+++ examples/base/variables.tf
@@ -0,0 +1,54 @@
+variable "port_client_id" {
+  type = string
+}
+
+variable "port_client_secret" {
+  type = string
+}
+
+variable "port_base_url" {
+  type = string
+  default = ""
+}
+
+variable "initialize_port_resources" {
+    type = bool
+    default = true
+    description = "If true, the module will initialize the default port resources (blueprints and relations)"
+}
+
+variable "integration_identifier" {
+    type = string
+    default = "azure"
+    description = "The identifier of the integration"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+  description = "The location to deploy the container to"
+}
+
+variable "image" {
+  type = string
+  default = ""
+  description = "The image that the integration will use, if not provided the module will use the latest image"
+}
+
+variable "additional_secrets" {
+  type = map(string)
+  default = {}
+  description = "Additional secrets to be injected into the container, the key of the map is the name of the secret and will be the environment variable name, e.g. {MY_SECRET: \"my-secret\"}"
+}
+
+variable "additional_environment_variables" {
+  type = map(string)
+  default = {}
+  description = "Additional environment variables to be injected into the container"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = null
+  description = "The resource group to deploy the container to and where the role definition will be created"
+}
